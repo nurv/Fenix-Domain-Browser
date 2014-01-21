a0540b98ab81374ff8e0ae010161737144d80689
sergiofbsilva <sergiofbsilva@gmail.com>
2014-01-21 20:29:16 +0000

    use a directory where a process runs mvn clean ff:package-dmls periodically instead of hardcoded links

--- src/fenixDomainBrowser/server/ConfigurationProperties.java
+++ src/fenixDomainBrowser/server/ConfigurationProperties.java
@@ -1,5 +1,7 @@
 package fenixDomainBrowser.server;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -7,30 +9,43 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.commons.lang.StringUtils;
+
 public class ConfigurationProperties {
-	private static Properties props;
-	
-	
-	static {
-		props = new Properties();
-		try {
-			props.load(ConfigurationProperties.class.getResourceAsStream("/configuration.properties"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static Map<String, URL> getDMLUrls() {
-		Map<String,URL> urls = new HashMap<String,URL>();
-		Integer max = Integer.parseInt((String) props.get("dml.urls.max"));
-		for(int i=1; i <= max; i++) {
-			try {
-				String url = (String) props.get("dml.urls." + i);
-				urls.put(String.valueOf(i), new URL(url));
-			} catch (MalformedURLException e) {
-				e.printStackTrace();
-			}
-		}
-		return urls;
-	}
+    private static Properties props;
+
+    static {
+        props = new Properties();
+        try {
+            props.load(ConfigurationProperties.class.getResourceAsStream("/configuration.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Map<String, URL> getDMLUrls() {
+        Map<String, URL> urls = new HashMap<String, URL>();
+        String dmlPath = (String) props.get("dml.path");
+
+        if (StringUtils.isEmpty(dmlPath)) {
+            dmlPath = "/tmp/dmls/";
+        }
+
+        File[] listFiles = new File(dmlPath).listFiles(new FilenameFilter() {
+
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".dml");
+            }
+        });
+
+        for (File file : listFiles) {
+            try {
+                urls.put(file.getName(), file.toURL());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+        return urls;
+    }
 }
